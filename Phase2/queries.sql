b271ce81a67f293478644efe4d6674e1862b6b36
YousefAtefB <YousefAtef.b.a.t@gmail.com>
2023-01-03 16:04:14 +0200

    queries

---
+++ Phase2/queries.sql
@@ -0,0 +1,7 @@
+-- first name and last name of male employees where salary > 10000 and have a female dependents and don't manage a department
+
+-- distinct names of customers that have projects with deadline > 2020 that is controlled by a department that have a female manager
+
+-- all info of departments that have #employees > 10 that control #projects > 1 
+
+-- name of department manager and name of department where that control project that has location = "dummy"  and customer name = "dummy"
