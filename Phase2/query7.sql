420c278f54c71e354c2379dd625bfd4ddc311d40
mohamed99akram <mohamed1999akram@gmail.com>
2023-01-04 00:29:10 +0200

    q7

---
+++ Phase2/query7.sql
@@ -0,0 +1,5 @@
+select  DEPENDENT_NAME 
+from dependent
+join employee on employee.ssn = dependent.ESSN
+join department on department.dep_number = employee.works_for
+where department.dep_name = "tfdxeafftatcjuajl"
